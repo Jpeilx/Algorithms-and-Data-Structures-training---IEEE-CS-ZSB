e42a847285395a495cf05ff05fa5a70216630e9b
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-02-22 15:06:20 +0200

    Create problem3.cpp

---
+++ Task-1/problem3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n ;
+    cin>>n ;
+    cout<<"Feliz nat";
+    while (n--){
+        cout<<'a' ;
+    }
+    cout<<"l!"<<endl ; 
+
+ 
+    return 0;
+}
