c00569d4bd3e5756366559c73ea1fea862f2bd2d
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-02-22 16:28:34 +0200

    Create problem6.cpp

---
+++ Task-1/problem6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+ 
+int main() {
+ 
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int c ;
+    cin>>c ;
+    vector<string>s(c) ;
+    for (int i = 0 ; i<c ; i++)
+        getline(cin,s[i]) ;
+    for (int i = 0 ; i<c ; i++)
+        cout<<"gzuz\n";
+}
