50a105508f500827fa6e5fa7fb1ce1918343aeee
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-03-02 17:05:19 +0200

    Create problem5.cpp

---
+++ Task-3/problem5.cpp
@@ -0,0 +1,35 @@
+//  In the name of of Allah  //
+//O Allah, peace and blessings upon the Prophet Muhammad//
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <cmath>
+#include <iomanip>
+#include <set>
+using namespace std ;
+#define ll  long long
+#define v  vector<int>
+#define l list<int>
+#define all(x) x.begin(), x.end()
+#define xx(i,a,n) for (int i = a ; i<n ; i++)
+int main () {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n ,out = 0;
+    string s;
+    cin>>n>>s ;
+    for (int i = 0 ; i<s.length()-1 ; i++){
+        if (s[i]==s[i+1]){
+            s.erase(s.begin()+i);
+            out++;
+            i--;
+        }
+    }
+    cout<<out;
+}
