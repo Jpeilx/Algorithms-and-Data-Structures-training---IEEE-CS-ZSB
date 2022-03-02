e5dccb8fec73c8650c4b5ea09a58884fb315e4b2
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-03-02 18:46:27 +0200

    Create problem4.cpp

---
+++ Task-3/problem4.cpp
@@ -0,0 +1,33 @@
+                          //  In the name of of Allah  //
+                //O Allah, peace and blessings upon the Prophet Muhammad//
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <cmath>
+#include <iomanip>
+#include <set>
+using namespace std ;
+#define ll  long long
+#define v  vector<int>
+#define l list<int>
+#define all(x) x.begin(), x.end()
+#define xx(i,a,n) for (int i = a ; i<n ; i++)
+int main () {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    string s , t ;
+    cin>>s>>t ;
+    int output=0;
+    for (int i = 0 ; i<t.length();i++){
+        if (t[i]==s[output])
+            output++;
+    }
+    cout<<output+1;
+
+}
