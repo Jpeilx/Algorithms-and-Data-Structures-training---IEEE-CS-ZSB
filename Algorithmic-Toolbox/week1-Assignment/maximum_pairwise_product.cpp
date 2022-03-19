43b9e64c8e4c6356cf9bb38919daf87812244e2d
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-03-19 17:02:10 +0200

    Create maximum_pairwise_product.cpp

---
+++ Algorithmic-Toolbox/week1-Assignment/maximum_pairwise_product.cpp
@@ -0,0 +1,25 @@
+                                 //  In the name of of Allah  //
+                      //O Allah, peace and blessings upon the Prophet Muhammad//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std ;
+int main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n ,x;
+    long long result ;
+    vector <int> vv ;
+    cin>>n ;
+    for (int i = 0 ;i<n ; i++){
+        cin>>x ;
+        vv.push_back(x);
+    }
+    sort(vv.begin(),vv.end());
+    result=vv[vv.size()-1]*vv[vv.size()-2];
+    cout<<result;
+}
+
+
+
