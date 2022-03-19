06bbf82c64c2d89b1d8a3f5c31d5ce41027c9ff0
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-03-19 16:49:12 +0200

    Create AplusB.cpp

---
+++ Algorithmic Toolbox/week-1 Assignment/AplusB.cpp
@@ -0,0 +1,14 @@
+                                 //  In the name of of Allah  //
+                      //O Allah, peace and blessings upon the Prophet Muhammad//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std ;
+int main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int a , b ;
+    cin>>a>>b ; 
+    cout<<a+b ; 
+}
