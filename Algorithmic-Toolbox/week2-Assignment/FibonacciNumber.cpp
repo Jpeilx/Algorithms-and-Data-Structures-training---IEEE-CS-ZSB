1f17adae3c40d891fcc3ccd9227ec672d68a87de
Mohammed Jbeil <96302371+Jpeilx@users.noreply.github.com>
2022-07-18 18:05:57 +0200

    Update FibonacciNumber.cpp

--- Algorithmic-Toolbox/week2-Assignment/FibonacciNumber.cpp
+++ Algorithmic-Toolbox/week2-Assignment/FibonacciNumber.cpp
@@ -1,20 +1,28 @@
-                                    //  In the name of of Allah  //
-                     //O Allah, peace and blessings upon the Prophet Muhammad//
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <list>
-#include <sstream>
-#include <string>
-#include <iterator>
-#include <cmath>
-#include <iomanip>
-#include <set>
-#include <map>
-#include <numeric>
-using namespace std ;
-long long  fib (int n ){
+#include <cassert>
+
+// The following code calls a naive algorithm for computing a Fibonacci number.
+//
+// What to do:
+// 1. Compile the following code and run it on an input "40" to check that it is slow.
+//    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
+// 2. Implement the fibonacci_fast procedure.
+// 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,
+//    uncomment the line with a call to test_solution, compile the program, and run it.
+//    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
+// 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
+// 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
+//    and submit it to the grader.
+
+int fibonacci_naive(int n) {
+    if (n <= 1)
+        return n;
+
+    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+}
+
+int fibonacci_fast(int n) {
+    // write your code here
     vector <long long > fibnum  ;
     fibnum.push_back(0) ;
     fibnum.push_back(1);
@@ -22,12 +30,23 @@ long long  fib (int n ){
         fibnum.push_back(fibnum[i-2]+fibnum[i-1]);
     }
     return fibnum[n];
-}
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    int n ;
-    cin>>n ;
-    cout<< fib(n);
 
+    return 0;
+}
+
+void test_solution() {
+    assert(fibonacci_fast(3) == 2);
+    assert(fibonacci_fast(10) == 55);
+    for (int n = 0; n < 20; ++n)
+        assert(fibonacci_fast(n) == fibonacci_naive(n));
+}
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+
+    std::cout << fibonacci_naive(n) << '\n';
+    //test_solution();
+    //std::cout << fibonacci_fast(n) << '\n';
+    return 0;
 }
