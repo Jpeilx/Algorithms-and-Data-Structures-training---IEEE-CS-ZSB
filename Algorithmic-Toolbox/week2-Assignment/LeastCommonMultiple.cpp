ac6ebcdd0526530f542b2af9d77615c53095a728
Mohammed Jbeil <96302371+Jpeilx@users.noreply.github.com>
2022-07-19 14:04:00 +0200

    Update LeastCommonMultiple.cpp

--- Algorithmic-Toolbox/week2-Assignment/LeastCommonMultiple.cpp
+++ Algorithmic-Toolbox/week2-Assignment/LeastCommonMultiple.cpp
@@ -1,32 +1,20 @@
-                                    //  In the name of of Allah  //
-                     //O Allah, peace and blessings upon the Prophet Muhammad//
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <list>
-#include <sstream>
-#include <string>
-#include <iterator>
-#include <cmath>
-#include <iomanip>
-#include <set>
-#include <map>
 #include <numeric>
-using namespace std ;
-int  greatestCommonDivisor (int a , int b  ){
-    if (b==0)
-        return  a ;
-    return greatestCommonDivisor(b,a%b);
+long long lcm_naive(int a, int b) {
+  for (long l = 1; l <= (long long) a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
 
+  return (long long) a * b;
 }
-long long LeastCommonMultiple(int a , int b  ){
-    return a*b/ greatestCommonDivisor(a,b);
+long long lcm_fast(long long  a , long long  b ){
+    return a*b/gcd(a,b);
 }
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    int a , b ;
-    cin>>a>>b ;
-    cout<<LeastCommonMultiple(a,b ) ;
+
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << lcm_fast(a, b) << std::endl;
+  return 0;
 }
