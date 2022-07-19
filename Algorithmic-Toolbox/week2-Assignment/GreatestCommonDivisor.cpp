ff4a15e94cb92c6330619950a2e28364f603ad5a
Mohammed Jbeil <96302371+Jpeilx@users.noreply.github.com>
2022-07-19 13:43:20 +0200

    Update GreatestCommonDivisor.cpp

--- Algorithmic-Toolbox/week2-Assignment/GreatestCommonDivisor.cpp
+++ Algorithmic-Toolbox/week2-Assignment/GreatestCommonDivisor.cpp
@@ -1,29 +1,26 @@
-                                    //  In the name of of Allah  //
-                     //O Allah, peace and blessings upon the Prophet Muhammad//
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <list>
-#include <sstream>
-#include <string>
-#include <iterator>
-#include <cmath>
-#include <iomanip>
-#include <set>
-#include <map>
-#include <numeric>
-using namespace std ;
-long long  greatestCommonDivisor (int a , int b  ){
+
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
+    }
+  }
+  return current_gcd;
+}
+int gcd_fast(int a , int b ){
     if (b==0)
         return  a ;
     return greatestCommonDivisor(b,a%b);
 
 }
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    int a , b ;
-    cin>>a>>b ;
-    cout<<greatestCommonDivisor(a, b);
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << gcd_fast(a, b) << std::endl;
+  return 0;
 }
