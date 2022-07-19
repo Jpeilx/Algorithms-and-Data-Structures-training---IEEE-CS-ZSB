16f8fad066088f7f3e1ec109cb3c470e2651f94e
Mohammed Jbeil <96302371+Jpeilx@users.noreply.github.com>
2022-07-19 15:26:47 +0200

    Create fibonacci_partial_sum.cpp

---
+++ Algorithmic-Toolbox/week2-Assignment/fibonacci_partial_sum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+
+long long get_fibonacci_partial_sum_naive(long long from, long long to) {
+    long long sum = 0;
+
+    long long current = 0;
+    long long next  = 1;
+
+    for (long long i = 0; i <= to; ++i) {
+        if (i >= from) {
+            sum += current;
+        }
+
+        long long new_current = next;
+        next = next + current;
+        current = new_current;
+    }
+
+    return sum % 10;
+}
+long long get_fibonacci_partial_sum_fast(long long from, long long to) {
+    long long sum = 0;
+
+    long long current = 0;
+    long long next  = 1;
+
+    for (long long i = 0; i <= to; ++i) {
+        if (i >= from) {
+            sum = (sum +current)%10;
+        }
+
+        long long new_current = next%10;
+        next = (next + current)%10;
+        current = new_current;
+    }
+
+    return sum ;
+}
+
+int main() {
+    long long from, to;
+    std::cin >> from >> to;
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
+}
